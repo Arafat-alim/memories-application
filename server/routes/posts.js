82a3b1605e01bf2e0f1dae41085ec6c40b6f9d1c
arafat-alim <arafat97alim@gmail.com>
2022-09-13 00:02:41 +0530

    added controller for more code readable and add functionality.l

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from "express";
+import { getPosts } from "../controller/posts.js";
+
 const router = express.Router();
 
 //! creating a routes
 // localhost:5000/posts
-router.get("/", (req, res) => {
-  res.send("Hurrah");
-});
+router.get("/", getPosts);
+
 // router.post();
 // router.put();
 // router.delete();
