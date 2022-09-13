c74b53eefb0e232ab6490fbffc3f39f5d45c3434
arafat-alim <arafat97alim@gmail.com>
2022-09-13 18:58:35 +0530

    created backend fetch and create post request

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,3 +1,24 @@
-export const getPosts = (req, res) => {
-  res.send("Hello Bhai");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    //find a post
+    const postMsgs = await PostMessage.find();
+    res.status(200).json(postMsgs);
+  } catch (error) {
+    //if error  found
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  //we need data from the frontend --> req.body
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(421).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getPosts } from "../controller/posts.js";
+import { getPosts, createPost } from "../controller/posts.js";
 
 const router = express.Router();
 
 //! creating a routes
 // localhost:5000/posts
 router.get("/", getPosts);
+router.post("/", createPost);
 
 // router.post();
 // router.put();
