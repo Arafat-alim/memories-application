2e5e46072e99da239acb89c532ee0c0510dd3f63
arafat-alim <arafat97alim@gmail.com>
2022-09-13 20:06:33 +0530

    frontend got styles.

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,22 @@ import { Container, AppBar, Grid, Typography, Grow } from "@material-ui/core";
 import memoriez from "./images/memories.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
   return (
     <Container maxidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memoriez} alt="memories" height="60" />
+        <img
+          className={classes.image}
+          src={memoriez}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,7 +1,7 @@
 import React from "react";
+import useStyles from "./styles";
 
-function Post() {
-  return <div>Post</div>;
-}
-
-export default Post;
+const Post = () => {
+  const classes = useStyles();
+  return <h1>Post</h1>;
+};
